b57985c4af01b34a6c025122844861b1db558841
Allan CORNET <nelson.numerical.computation@gmail.com>
2025-04-15 19:43:12 +0200

    Qt 5.15.2 Windows x86 (32-bit) Binaries

---
+++ 5.15.2/msvc2019/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Setting up environment for Qt usage...
+set PATH=D:\Developpements\Qt\5.15.2\msvc2019\bin;%PATH%
+cd /D D:\Developpements\Qt\5.15.2\msvc2019
+echo Remember to call vcvarsall.bat to complete environment setup!
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Qt 5.15.2 Windows x86 (32-bit) Binaries
+
+This repository provides precompiled **Qt 5.15.2** binaries for **Windows (x86 / 32-bit)**.
+
